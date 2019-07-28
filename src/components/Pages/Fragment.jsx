1c09369faad2c868c4408ac6ec4fa21db7f65bf0
Osidel <osidel79@gmail.com>
2019-07-28 07:29:36 -0700

    Configura los componentes  de rutas publicas y privadas y las rutas default

---
+++ src/components/Pages/Fragment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fragment = () => (
+    <div className="ed-grid">
+      <h1>Clase </h1>
+    </div>
+)
+
+export default Fragment
---
+++ src/components/Pages/Page404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page404 = () => (
+    <div className="ed-grid">
+      <h1>not found</h1>
+    </div>
+)
+
+export default Page404
